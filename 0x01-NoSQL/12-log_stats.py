3f5f9c5e1260e2f6865b6260c4b98df5cd8d995e
StevenAwo <rotimiawomodu@gmail.com>
2024-04-30 19:56:16 +0100

    TASK 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -21,10 +21,10 @@ def log_stats(mongo_collection, option=None):
     resultt = mongo_collection.count_documents(itemms)
     print(f"{resultt} logs")
     print("Methods:")
-    for methodd in METHODS:
-        log_stats(nginx_collection, methodd)
-    statuss_checkk = mongo_collection.count_documents({"path": "/status"})
-    print(f"{statuss_checkk} status check")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
