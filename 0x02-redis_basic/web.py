17eaa337465f6e3dd22cbf52885f621f62a82ba8
StevenAwo <rotimiawomodu@gmail.com>
2024-05-01 18:43:35 +0100

    TASK 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
+"""Using a web cache and also a tracker"""
 import requests
+
 import redis
+
 from functools import wraps
 
 store = redis.Redis()
 
 
 def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
+    """ Decorator thats for counting the nuber of
+    times that a URL has been accessed """
     @wraps(method)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+        cacheded_keyy = "cached:" + url
+        web_cached_data = store.get(cacheded_keyy)
+        if web_cached_data:
+            return web_cached_data.decode("utf-8")
 
-        count_key = "count:" + url
+        counting_key = "count:" + url
         html = method(url)
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
+        store.incr(counting_key)
+        store.set(cacheded_keyy, html)
+        store.expire(cacheded_keyy, 10)
         return html
     return wrapper
 
 
 @count_url_access
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """ Returning the HTML's content of the specific url """
+    ress = requests.get(url)
+    return ress.text
+
