ec622e249d1149961c73acfaf42c088e0503cea2
StevenAwo <rotimiawomodu@gmail.com>
2024-05-01 18:42:32 +0100

    TASK 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 """
-Using a web cache and also a tracker
+web cache and tracker
 """
-
 import requests
-
 import redis
-
 from functools import wraps
 
 store = redis.Redis()
 
 
 def count_url_access(method):
-    """ Decorator thats for counting the nuber of
-    times that a URL has been accessed """
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
     def wrapper(url):
-        cacheded_keyy = "cached:" + url
-        cached_data = store.get(cacheded_keyy)
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
 
@@ -26,8 +23,8 @@ def count_url_access(method):
         html = method(url)
 
         store.incr(count_key)
-        store.set(cacheded_keyy, html)
-        store.expire(cacheded_keyy, 10)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
         return html
     return wrapper
 
@@ -37,4 +34,3 @@ def get_page(url: str) -> str:
     """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
-
